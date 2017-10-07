fb4c98b7c7916506e670641952e7012a90fac594
Yuki Ino <tensai.osaru25035@gmail.com>
2017-10-07 07:43:56 +0000

    user login

--- db/migrate/20171005152423_add_user_to_task.rb
+++ db/migrate/20171005152423_add_user_to_task.rb
@@ -1,5 +1,12 @@
 class AddUserToTask < ActiveRecord::Migration[5.0]
   def change
-    add_column :tasks, :user, :reference
+    # add_column :tasks, :user, :reference
+    add_reference :tasks, :user, foreign_key: true
+  end
+end
+class AddUserToTask < ActiveRecord::Migration[5.0]
+  def change
+    # add_column :tasks, :user, :reference
+    add_reference :tasks, :user, foreign_key: true
   end
 end
